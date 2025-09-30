d037eb6040b3ade69f9d3c9fef63b264b017a232
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-09-30 07:58:57 -0700

    try

--- .github/workflows/assign-issues.js
+++ .github/workflows/assign-issues.js
@@ -10,7 +10,7 @@ const TEAM_ASSIGNMENTS = {
 
 async function run() {
   try {
-    const token = core.getInput('github-token', { required: true });
+    const token = core.getInput('github_token', { required: true });
     const octokit = github.getOctokit(token);
 
     const { context } = github;
